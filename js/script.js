ab8e1199bd2cade301bb102734e2fb9ed0b6f8d8
Manuela Scotto <manuelascotto18@gmail.com>
2020-04-08 10:47:16 +0200

    migliorie

---
+++ js/script.js
@@ -0,0 +1,38 @@
+alert('HELLO SPOTIFY WORLD!');
+document.getElementById('creato').innerHTML='Creato per Manuela Scotto';
+document.getElementById('name').innerHTML='Manuela Scotto';
+
+console.log('pippo');
+
+// VARIABILE METODO 1
+var number = 5;
+
+console.log(number);
+
+// VARIABILE METODO 2
+var miaVariabile;
+
+console.log(miaVariabile);
+
+// a questo punto basta mettere il nome della variabile senza dover riscrivere var
+miaVariabile = 'hello';
+
+console.log(miaVariabile);
+
+miaVariabile = 'arrivederci';
+
+console.log(miaVariabile);
+
+// NUMERI
+var somma = 4 + 4;
+console.log(somma);
+
+var sottr = somma - 2 ;
+console.log(sottr);
+
+var molt = somma * sottr;
+console.log(molt);
+
+// PROMPT
+var userText = prompt('qual è il tuo nome');
+console.log(userText);
