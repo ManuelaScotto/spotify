d5b26a5d84f570925468221d3d469d564ec8bc10
Manuela Scotto <manuelascotto18@gmail.com>
2020-04-09 18:29:27 +0200

    js

--- js/script.js
+++ js/script.js
@@ -5,33 +5,3 @@ console.log(userText);
 document.getElementById('name').innerHTML= userText;
 
 document.getElementById('creato').innerHTML='Creato per ' + userText;
-
-
-// VARIABILE METODO 1
-var number = 5;
-
-console.log(number);
-
-// VARIABILE METODO 2
-var miaVariabile;
-
-console.log(miaVariabile);
-
-// a questo punto basta mettere il nome della variabile senza dover riscrivere var
-miaVariabile = 'hello';
-
-console.log(miaVariabile);
-
-miaVariabile = 'arrivederci';
-
-console.log(miaVariabile);
-
-// NUMERI
-var somma = 4 + 4;
-console.log(somma);
-
-var sottr = somma - 2 ;
-console.log(sottr);
-
-var molt = somma * sottr;
-console.log(molt);
